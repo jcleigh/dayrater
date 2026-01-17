e17a12253ce5dd714138bfa5b46cdb64d47cf313
jcleigh <cleigh.jordan@gmail.com>
2026-01-17 17:10:58 -0600

    refactor: Simplify TrendLineChart implementation and improve data handling

--- app/src/main/java/com/dayrater/ui/insights/components/TrendLineChart.kt
+++ app/src/main/java/com/dayrater/ui/insights/components/TrendLineChart.kt
@@ -2,22 +2,21 @@ package com.dayrater.ui.insights.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.dayrater.domain.model.TrendData
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
-import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottomAxis
-import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStartAxis
+import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
+import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
-import com.patrykandpatrick.vico.compose.common.component.rememberLineComponent
+import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
+import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
-import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer
 
 /**
  * Line chart component for displaying rating trends using Vico library.
@@ -36,32 +35,20 @@ fun TrendLineChart(
         return
     }
     
-    // Update model with data
-    remember(trendData) {
+    // Update model with data using LaunchedEffect for suspend function
+    LaunchedEffect(trendData) {
         modelProducer.runTransaction {
             lineSeries {
-                series(dataPoints.map { it.value!! })
+                series(dataPoints.map { it.value!!.toDouble() })
             }
         }
     }
     
     CartesianChartHost(
         chart = rememberCartesianChart(
-            rememberLineCartesianLayer(
-                lineProvider = LineCartesianLayer.LineProvider.series(
-                    LineCartesianLayer.rememberLine(
-                        remember {
-                            LineCartesianLayer.LineFill.single(
-                                com.patrykandpatrick.vico.core.common.Fill(
-                                    android.graphics.Color.parseColor("#4CAF50")
-                                )
-                            )
-                        }
-                    )
-                )
-            ),
-            startAxis = rememberStartAxis(),
-            bottomAxis = rememberBottomAxis()
+            rememberLineCartesianLayer(),
+            startAxis = VerticalAxis.rememberStart(),
+            bottomAxis = HorizontalAxis.rememberBottom()
         ),
         modelProducer = modelProducer,
         modifier = modifier
